ea034de0fb1d6a9cfcdb34d1d63057461eb1fd57
Leandro Brunner <leandrobrunner@gmail.com>
2026-02-08 01:36:46 -0300

    chore: update package.json for module exports and dependencies; enhance rollup config for multiple outputs; add tsconfig.json for TypeScript support

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,64 @@
-import terser from '@rollup/plugin-terser';
-import copy from 'rollup-plugin-copy';
+import terser from "@rollup/plugin-terser";
+import copy from "rollup-plugin-copy";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: 'force-app/main/default/lwc/signals/signals.js',
-  output: {
-    file: 'dist/signals/signals.js',
-    format: 'es',
-    sourcemap: false,
+const input = "force-app/main/default/lwc/signals/signals.js";
+
+export default [
+  {
+    input: "force-app/main/default/lwc/signals/signals.js",
+    output: {
+      file: "dist/signals/signals.js",
+      format: "es",
+      sourcemap: false,
+    },
+    plugins: [
+      terser({
+        mangle: {
+          properties: true,
+        },
+      }),
+      copy({
+        targets: [
+          {
+            src: "force-app/main/default/lwc/signals/signals.js-meta.xml",
+            dest: "dist/signals",
+          },
+        ],
+      }),
+    ],
   },
-  plugins: [
-    terser(),
-    copy({
-      targets: [
-        { src: 'force-app/main/default/lwc/signals/signals.js-meta.xml', dest: 'dist/signals' }
-      ]
-    })
-  ]
-};
+  {
+    input,
+    output: {
+      file: "dist/index.js",
+      format: "es",
+    },
+    plugins: [
+      terser({
+        mangle: {
+          properties: true,
+        },
+      }),
+    ],
+  },
+  {
+    input,
+    output: {
+      file: "dist/index.cjs",
+      format: "cjs",
+    },
+    plugins: [
+      terser({
+        mangle: {
+          properties: true,
+        },
+      }),
+    ],
+  },
+  {
+    input,
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
